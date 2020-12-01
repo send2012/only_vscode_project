3fb095245e94c11add5566cfaa7a3623f590e289
send2012 <wangufovdjyu@outlook.com>
2020-12-01 12:08:46 +0800

    Organize related documents

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.use(VueRouter)
+import Entrance from '@/views/entrance/';
+import BuildInPage from '@/views/buildIn_page/';
+
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	{
+		path: '/',
+        name: 'entrance',
+        // route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: /* webpackChunkName: "Entrance" */ Entrance,
+    },
+    {
+        path: '/buildIn-page',
+        name: 'buildIn_page',
+        component: /* webpackChunkName: '' */ BuildInPage,
+    },
+];
 
 const router = new VueRouter({
-  routes
-})
+	routes,
+});
 
-export default router
+export default router;
---
+++ src/views/entrance/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="entrance">
+		<span>entrance</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'entrance',
+	};
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
